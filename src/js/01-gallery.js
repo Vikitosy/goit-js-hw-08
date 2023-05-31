b00d2554b9f9168b695ee9aa609300f38c79a6ad
Vikitosy <120407265+Vikitosy@users.noreply.github.com>
2023-05-31 08:39:12 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
-console.log("hi there");
+
+const galleryContainer = document.querySelector('.gallery');
+const cardMarkup = createGalleryCard(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createGalleryCard(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+        `;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox');
+
+
+
+
+// Change code below this line
+
+
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('input', throttle(onTextareaInput, 500));
+
+initForm();
+
+function onTextareaInput(evt) {
+  let parsedUserData = localStorage.getItem('feedback-form-state');
+  parsedUserData = parsedUserData ? JSON.parse(parsedUserData) : {};
+
+  parsedUserData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(parsedUserData));
+}
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const { email, message } = evt.currentTarget.elements;
+ console.log({ email: email.value, message: message.value });
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+function initForm() {
+  savedUserData = localStorage.getItem('feedback-form-state');
+
+  if (savedUserData) {
+    const parsedUserData = JSON.parse(savedUserData);
+    Object.entries(parsedUserData).forEach(([name, value]) => {
+      feedbackForm.elements[name].value = value;
+    });
+  }
+}
